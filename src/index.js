17f638dd946a3b7e11d356ed4f1a7074bc13249b
Oli Fog <oliver@fogelin.com>
2020-12-30 20:09:50 +0000

    Initial bot framework

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+const { Client } = require('discord.js');
+const { registerCommands, registerEvents } = require('./utils/registry');
+const config = require('../config.json');
+const client = new Client();
+
+(async () => {
+  client.commands = new Map();
+  client.events = new Map();
+  await registerCommands(client, '../commands');
+  await registerEvents(client, '../events');
+  await client.login(config.token);
+})();
+
